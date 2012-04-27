fe41799fa815790ccee35ec40fdceaa2eb718b41
Raoul Millais <raoul@raoulmillais.com>
2012-04-27 15:21:02 +0100

    Stub release endpoints and move artist recommend

--- server.js
+++ server.js
@@ -16,7 +16,14 @@ server.use(function addDefaultHeaders(req, res, next) {
 server.get('/artist/bytag/top', conventions.tags);
 server.get('/artist/search', conventions.search);
 server.get('/artist/similar', conventions.id);
+server.get('/artist/recommend', conventions.id);
 server.get('/artist/tags', conventions.id);
+
+server.get('/release/details', conventions.id);
+server.get('/release/recommend', conventions.id);
+server.get('/release/search', conventions.search);
+server.get('/release/tags', conventions.id);
+server.get('/release/tracks', conventions.id);
 
 server.listen(+process.env.PORT || 3000, function serverListening() {
 	console.log('Server listening on %s', +process.env.PORT || 3000);
