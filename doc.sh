472c02345b2ab5238818da70b401a9165963e589
Raoul Millais <raoul@raoulmillais.com>
2012-06-12 12:53:40 +0100

    Remove erroneous comma from package json

--- doc.sh
+++ doc.sh
@@ -1,5 +1,1 @@
-#! /bin/bash
-
-md2html README.html > docs/content.html
-cd docs
-cat layout-top.html content.html layout-bottom.html > index.html
+md2html README.md |sed 's/\r$/\n/' > docs/content.html && cd docs && cat layout-top.html content.html layout-bottom.html | sed 's/\r$/\n/' > "./index.html"
