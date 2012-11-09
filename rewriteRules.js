b9fc1af6cba641575a586ae002795a6806f4365c
Henry Oswald <henry.oswald@7digital.com>
2012-11-09 15:28:03 +0000

    can do path rewrites

--- rewriteRules.js
+++ rewriteRules.js
@@ -2,14 +2,20 @@ var request = require('request');
 var path = require("path");
 module.exports = {
   apply:function(server, rules){
-    server.use(function(req, res, next){
-      //console.log(request);
-      console.log(request.get)
-      if(req.host){
-	      request.get(rules.host+req.url).pipe(res);
-      }
-      
-      next();
-    });
+	server.use(function(req, res, next){
+	  if(rules.urlRewrites){
+		  rules.urlRewrites.forEach(function(rule){
+			if(rule.requestedUrl === req.url){
+			  console.log("proxying from "+rule.requestedUrl+" to "+rule.replacementUrl);
+			  request.get(rule.replacementUrl).pipe(res);
+			  return;
+			};
+		  });
+	  }else if(rules.host){
+		  request.get(rules.host+req.url).pipe(res);
+	  }else{
+		  next();
+	  }
+	});
   }
 };
--- spec/rewrite_rules.js
+++ spec/rewrite_rules.js
@@ -16,7 +16,7 @@ describe("rewriting requested urls", function(){
   });
 
   it("should not do any rewriting for no rules", function(done){
-    this.stubRequest.get = sinon.spy()
+    this.stubRequest.get = sinon.spy();
     var server = {
       use:function(callback){
         callback({host:""}, {}, done);
@@ -26,7 +26,7 @@ describe("rewriting requested urls", function(){
   });
 
   it("should pipe request to new host when host change rule is implemented", function(done){
-    var newHost = "http://new.host.com"
+    var newHost = "http://new.host.com";
     var req = {host:"http://www.bla.com", url:"/going/here?param=important"};
     var res = {};
     var pipeSpy = sinon.spy();
@@ -44,4 +44,26 @@ describe("rewriting requested urls", function(){
     };
     this.rewriteRules.apply(server, {host:newHost});
   });
+
+  it("should pipe to different url when given rule", function(done){
+    var newUrl = "http://new.host.com/somewhere/new?please=true";
+    var oldPath = "/old/place";
+    var req = {host:"http://www.bla.com", url:oldPath};
+    var res = {};
+    var pipeSpy = sinon.spy();
+    this.stubRequest.get = function(url){
+      url.should.equal(newUrl);
+      return{ pipe: function(desination){
+        desination.should.equal(res);
+        done();
+      }};
+    };
+    var server = {
+      use:function(callback){
+        callback(req, res, function(){});
+      }
+    };
+    this.rewriteRules.apply(server, {urlRewrites:[{requestedUrl: oldPath, replacementUrl: newUrl}]});
+  });
+
 });
